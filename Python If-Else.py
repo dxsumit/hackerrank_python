d26fc5d814e2b2db904fb612f2106304ca4f78ed
dxsumit <58907200+dxsumit@users.noreply.github.com>
2020-06-26 11:41:46 +0530

    Create Python If-Else.py

---
+++ Python If-Else.py
@@ -0,0 +1,11 @@
+# taking input 
+n= int(input())
+
+if(n%2!=0):
+    print("Weird")
+elif(n%2==0 and n>20):
+    print("Not Weird")
+elif(n%2==0 and 1<n<6):
+    print("Not Weird")
+elif(n%2==0 and 5<n<21):
+    print("Weird")
