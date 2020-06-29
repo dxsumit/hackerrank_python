4359f7207e61a898be9f24040a262fe90a53d159
Sumit Kumar <58907200+dxsumit@users.noreply.github.com>
2020-06-29 11:16:19 +0530

    Create Capitalize.py

---
+++ Capitalize.py
@@ -0,0 +1,15 @@
+def solve(s):
+    for each in s.split():
+       s=s.replace(each , each.capitalize())
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
